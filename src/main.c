f8cd245546c376af2ad916db0f7faef55e546109
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-05-06 13:02:43 -0500

    Source C
    Se mejoró el menú de opciones con condiciones, para que de ese modo se puede efectuar correctamente la operación aritmética.

--- src/main.c
+++ src/main.c
@@ -6,13 +6,21 @@
 #include "include/op.h"
 #include "include/dataentry.h"
 
+enum
+{
+	DISABLE,
+	ENABLE
+};
+
 int main(void) 
 {
 	char option;
+	uint8_t act = ENABLE;
 	while(1)
 	{
 		puts("-> Operaciones entre matrices.");
 		puts("-> Menu de Opciones:");
+		printf("0. %s el ingreso de datos\n", (act == ENABLE) ? ("Desactivar") : ("Activar"));
 		puts("1. Sumar");
 		puts("2. Restar");
 		puts("3. Multiplicar");
@@ -21,39 +29,53 @@ int main(void)
 		puts("6. Sumar los bordes");
 		puts("7. Sumar en forma de T");
 		puts("8. Salir del programa");
-		dataread("%c", &option, "Ingrese una opcion <1-8>");
-		assertc(option >= '1' && option <= '8', "\nError: Ingrese una opcion valida.\n");
+		dataread("%c", &option, "Ingrese una opcion <0-8>");
+		assertc(option >= '0' && option <= '8', "\nError: Ingrese una opcion valida.\n")
+		if((act == ENABLE) && (option >= '1' && option <= '7'))
+		{
+			error(DataEntry, option);
+		}
+		else if((option != '0' && option != '8') && (!DetectRules(option)))
+		{
+			pauseprogram();
+			continue;
+		}
 		switch(option)
 		{
-			case '1':
-				error(DataEntry);
+			case '0':
+				assertc(getrows(MA) != 0, "\nError: Usted no ha ingresado ningun dato a las matrices A y B.\n")
+				act = !act;
+				printf("\nInfo: Has %s el ingreso de datos de ambas matrices (A y B)\n", (act == DISABLE) ? ("Desactivado") : ("Activado"));
+				break;
+			case ADD:
 				as_Matrix(ADD);
 				break;
-			case '2':
-				error(DataEntry);
-				as_Matrix(SUBTRACT);
+				
+			case SUB:
+				as_Matrix(SUB);
 				break;
-			case '3':
-				error(DataEntry);
+				
+			case MULTI:
 				multi_Matrix();
 				break;
-			case '4':
-				error(DataEntry);
+				
+			case MD:
 				Add_MD();
 				break;
-			case '5':
-				error(DataEntry);
+				
+			case RD:
 				Add_RD();
 				break;
-			case '6':
-				error(DataEntry);
+				
+			case EDGE:
 				Add_Edge();
 				break;
-			case '7':
-				error(DataEntry);
+				
+			case WAY_T:
 				Add_WayT();
 				break;
-			case '8':
+				
+			case EXIT_:
 				return EXIT_SUCCESS;
 		}
 		pauseprogram();
